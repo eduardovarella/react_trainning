1124e64b31e0069a38635a9b60ce00beb3224147
Eduado Varella <eduardo.varella@bizcap.com.br>
2018-06-21 09:23:28 -0300

    adding describe statements

--- react-redux-tutorial/testing/src/components/__tests__/CommentBox.test.js
+++ react-redux-tutorial/testing/src/components/__tests__/CommentBox.test.js
@@ -18,24 +18,26 @@ it('has a textarea and a button', () => {
     expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a textarea that users can type in', () => {
-    wrapped.find('textarea').simulate('change', {
-        target: {
-            value: 'new comment'
-        }
+describe('the text area', () => {
+
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: {
+                value: 'new comment'
+            }
+        });
+        wrapped.update();
     });
 
-    wrapped.update();
-
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
-
-it('has a form that when submitted clear the textarea', () => {
-    wrapped.find('textarea').simulate('change', {target: {value: 'new comment'}});
-    wrapped.update();
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    it('has a textarea that users can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    });
     
-    wrapped.find('form').simulate('submit');
-    wrapped.update();
-    expect(wrapped.find('textarea').prop('value')).toEqual('');
+    it('has a form that when submitted clear the textarea', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+        
+        wrapped.find('form').simulate('submit');
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
 });
