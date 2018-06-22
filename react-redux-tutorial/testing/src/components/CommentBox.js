e6bbb8fc8f6ec9fc27970cc9df6c3d2b80fbcba6
Eduado Varella <eduardo.varella@bizcap.com.br>
2018-06-22 12:58:33 -0300

    Adding a Root Component

--- react-redux-tutorial/testing/src/components/CommentBox.js
+++ react-redux-tutorial/testing/src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
 
@@ -10,10 +12,7 @@ class CommentBox extends Component {
 
     handleSubmit = event => {
         event.preventDefault();
-
-        // Call an action creator
-        // And save the comment
-
+        this.props.saveComment(this.state.comment);
         this.setState({comment: ''});
     };
 
@@ -31,4 +30,4 @@ class CommentBox extends Component {
 
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
