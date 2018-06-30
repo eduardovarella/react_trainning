4146dab98d52cf58ff3e815d46b1fda75b92b858
Eduardo Varella <evarella@gmail.com>
2018-06-30 11:43:17 -0300

    Udacity: State Management

--- udacity/contacts/src/App.js
+++ udacity/contacts/src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListContacts from './ListContacts';
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+  removeContact = (contact) => {
+    this.setState((state) => (
+      {
+        contacts: state.contacts.filter((c) => c.id !== contact.id)
+      }
+    ));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello World</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ListContacts 
+          contacts={ this.state.contacts }
+          onDeleteContact = {this.removeContact}
+          />
       </div>
-    );
+    )
   }
 }
 
